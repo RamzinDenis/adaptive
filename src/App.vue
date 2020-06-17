e871cff639f2248c73a9080ded8f0979fea4851f
RamzinDenis <denis.ramzin@mail.ru>
2020-06-17 09:25:47 +0300

    initial setup with normalize.css, sass loader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Donations />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Donations from "./views/Donations";
+import "normalize.css";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Donations
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/views/Donations.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from "@/components/Card";
+export default {
+  components: {
+    Card
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
