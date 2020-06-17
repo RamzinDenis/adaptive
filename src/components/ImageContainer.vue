5ac9447e8586c803f999fde01a16c3f4c75c8fc7
RamzinDenis <denis.ramzin@mail.ru>
2020-06-17 18:20:02 +0300

    add styles to ImageContainer

---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="img-container">
+    <div class="img-container__shadow"></div>
+
+    <div class="img-container__content">
+      <img class="img-container__title" src="../assets/images/davis-fund.png" />
+      <div class="img-container__geo">
+        <BaseIcon name="geo" />
+        <span class="img-container__geo-text">Омск</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.img-container {
+  background-image: url("../assets/images/children.png");
+  height: 180px;
+  @include flex;
+  position: relative;
+  &__content {
+    flex-direction: column;
+    @include flexBetween;
+    margin: 10px;
+  }
+  &__geo {
+    @include flexCenter;
+    &-text {
+      color: $white;
+      margin-left: 7px;
+      @include h5;
+    }
+  }
+  &__shadow {
+    height: 180px;
+    width: 100%;
+    position: absolute;
+    z-index: -2;
+    right: -20px;
+    bottom: -30px;
+    background-color: $invisible-grey;
+  }
+}
+</style>
